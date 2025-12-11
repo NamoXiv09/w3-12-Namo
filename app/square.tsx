08b55e33f57483dc16ce5b161a5a9581c43faa29
NamoXiv09 <namo.yawanopas@e-tech.ac.th>
2025-12-11 09:36:22 +0700

    first commit

---
+++ app/square.tsx
@@ -0,0 +1,50 @@
+import { View , Text, Button, StyleSheet, TextInput } from "react-native";
+import { useRouter } from "expo-router";
+import { useState } from "react";
+
+export default function Square(){
+
+    const router = useRouter()
+    const [width, setWidth] = useState(0)
+    const [lenght, setLenght] = useState(0)
+    const [area, setArea] = useState(0)
+
+    return(
+        <View style={styles.container}>
+            <Text style={styles.mainTitle}>คำนวณพื้นที่สี่เหลี่ยม</Text>
+            {/* <Button title="กลับหน้าแรก" onPress={() => router.navigate('/')} /> */}
+
+            <TextInput 
+                style={styles.textInput} 
+                placeholder="กรอกความกว้าง"
+                value={width.toString()}
+                onChangeText={(w) => setWidth(Number(w))}
+            />
+
+            <TextInput style={styles.textInput} placeholder="กรอกความยาว"/>
+
+            <Button title="คำนวณ"/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor:"white",
+        justifyContent:"center",
+        alignItems:"center",
+        gap:15
+    },
+
+    mainTitle:({
+        fontSize:20,
+        fontWeight:"700"
+    }),
+
+    textInput:{
+        borderWidth: 1,
+        width: "80%",
+        borderColor:"green"
+    }
+})
